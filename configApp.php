6ab35967456fdeaeb04911c28c17872b2ed5ec6a
mundotool <43940072+mundotool@users.noreply.github.com>
2018-11-27 13:22:00 -0600

    Update configApp.php

--- configApp.php
+++ configApp.php
@@ -1,15 +1,14 @@
 <?php
 
 /* Go to My Apps dashboard: https://developers.mercadolibre.com.mx/apps/home, and get the information you need in order to the following enviroment variables */
-
 /* Your Application Id */
-$appId = getenv('3457225608424475');
+$appId = getenv('App_ID');
 
 /* Your Secret Key */
-$secretKey = getenv('Dq4T8mrg6NWmtxoednuS7FDXSqNPQIE7');
+$secretKey = getenv('Secret_Key');
 
 /* The Redirect url */
-$redirectURI = getenv('https://www.mercadolibre.com.mx/');
+$redirectURI = getenv('Redirect_URI');
 
 /* The site id of the country where your application will work.
 If you don't know your site_id go to our sites resources: https://api.mercadolibre.com/sites  */
